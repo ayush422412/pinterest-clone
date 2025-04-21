cdae94dd21383e5efd3ff2c162813609d8adab1f
ayush422412 <ayush210yadav@gmail.com>
2025-04-21 22:27:08 +0530

    final commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,8 @@
 import Header from './components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Provider from './components/Provider'
 
-import Provider from './Provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
